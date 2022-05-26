9ccf690d8805c095b9fb079554b9c4673e6a50cf
@奇 <360141773@qq.com>
2022-05-26 18:06:41 +0800

    增加存储

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:zapi/components/APIGroup/data_model.dart';
@@ -5,13 +8,41 @@ import 'package:zapi/components/DataStorage/mod.dart';
 
 import 'package:zapi/forms/add_group.dart';
 import 'package:zapi/pages/group_page.dart';
+import 'package:zapi/utils/ext_widgets.dart';
 import 'package:zapi/utils/standard.dart';
 import 'test.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  initPrefs(); // 初始化存储实例
+  await initPrefs(); // 初始化存储实例
+  /// TESTING
   group.addApis(apiList);
+  await prefs.setString(group.name, jsonEncode(group));
+
+  await prefs.setStringList('groupNameList', [group.name]);
+
+  // 加载group列表 groupList
+  List<String>? groupNameList = prefs.getStringList('groupNameList');
+  for (String group in groupNameList!) {
+    String? groupStr = prefs.getString(group);
+    if (groupStr == null) continue;
+    try {
+      var test = jsonDecode(groupStr);
+      groupList.add(ApiGroup.fromJson(test));
+    } catch (e) {
+      prefs.remove(group);
+      print("error");
+    }
+  }
+
+  // groupList.add(group);
+  // String json = jsonEncode(group);
+  // print(json);
+  // var maps = jsonDecode(json);
+  // for (var map in maps) {
+  //   log(map.toString());
+  // }
+  // apiList[0] = ApiWidgetInfo.fromJson();
 
   EasyLoading.instance
     ..displayDuration = const Duration(milliseconds: 1200)
@@ -57,8 +88,9 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("${widget.title} API组")),
-      drawer: const Drawer(child: const SafeArea(child: SizedBox())),
-      body: Center(
+      drawer: const Drawer(child: SafeArea(child: SizedBox())),
+      body: CardContainer(
+        margin: const EdgeInsets.symmetric(horizontal: horMargin * 2, vertical: verMargin),
         child: ListView.builder(
             padding: const EdgeInsets.only(top: topPadding, bottom: bottomPadding),
             itemCount: groupList.length,
@@ -75,7 +107,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.of(context).push(MaterialPageRoute(builder: (context) => AddGroupForm()));
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) => const AddGroupForm()));
         },
         tooltip: '添加API组',
         child: const Icon(Icons.add),
